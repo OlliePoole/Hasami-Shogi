dbedc5c117ac8fde5f5907415769474fd667f996
oliver.t.poole <oliverpoole7@gmail.com>
2015-09-27 11:44:28 +0100

    Added view controller for registering users to storyboard

---
+++ Hasami Shogi/Controllers/HSRegisterUserViewController.swift
@@ -0,0 +1,13 @@
+//
+//  HSRegisterUserViewController.swift
+//  Hasami Shogi
+//
+//  Created by Oliver Poole on 27/09/2015.
+//  Copyright © 2015 OliverPoole. All rights reserved.
+//
+
+import UIKit
+
+class HSRegisterUserViewController: UIViewController {
+
+}
